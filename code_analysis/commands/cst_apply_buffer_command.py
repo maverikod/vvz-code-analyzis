1554d3b3d5065f289c7255552ab2992fbd635c3d
Vasiliy Zdanovskiy <vasilyvz@gmail.com>
2026-04-28 01:47:27 +0300

    cst_create_file: cst_apply_buffer_command.py

---
+++ code_analysis/commands/cst_apply_buffer_command.py
@@ -0,0 +1,12 @@
+"""MCP command: cst_apply_buffer
+
+Apply replacement code from a completed transfer upload buffer to a CST node.
+Avoids large payload in JSON-RPC by uploading code in advance via transfer API.
+
+Author: Vasiliy Zdanovskiy
+email: vasilyvz@gmail.com"""
+
+# cst-node-ids: begin
+# cst-node-ids: version=2
+# cst-node-ids: data={"0":"17e9e39a-1ccc-4c19-bb0c-5f5479ef2897","0.0":"4bfbeb9d-c853-4127-b4bc-72e2accf82c2","0.0.0":"21c0e8d5-dfa5-4585-b19b-caf99fe758eb","0.0.0.0":"f85576e7-8950-45f9-b44a-c309b3d64557","0.0.1":"006a315c-396b-48d9-a14c-636d9ab2e6d5","0.0.1.0":"e00d6465-eeae-4ccc-b8af-94d52d2c6cb7","0.0.1.1":"fd2a1d0f-1098-4ac4-ae11-d40cdd2efe3e"}
+# cst-node-ids: end

6f717aac5730dd653e316cb87c0d97e08a4b3360
Vasiliy Zdanovskiy <vasilyvz@gmail.com>
2026-04-29 10:10:56 +0300

    cst_create_file: tmp_verify_code.py

---
+++ tmp_verify_code.py
@@ -0,0 +1,6 @@
+"""Read actual source files for verification."""
+
+# cst-node-ids: begin
+# cst-node-ids: version=2
+# cst-node-ids: data={"0":"2a360eb1-2e3e-4e7e-b6d6-136a3c3d3a85","0.0":"0fbfdfa1-fc6e-493b-806c-4ecc4113f9db","0.0.0":"17d19d49-0587-482a-96a0-04dbbf4400ff","0.0.0.0":"509a5428-fff3-4461-9869-d9fa4a35c92f","0.0.1":"129b2b57-6eb6-41c1-96bf-317e5bb94372","0.0.1.0":"b9694b6b-c8be-4f55-ae5c-c41f1618ac3e","0.0.1.1":"2ad6904e-ad80-42c7-b73b-859c263cb142"}
+# cst-node-ids: end

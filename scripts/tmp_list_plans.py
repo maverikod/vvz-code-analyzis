c22d6b33ef3290194bc722a180e1bfb5f62cefc1
Vasiliy Zdanovskiy <vasilyvz@gmail.com>
2026-04-30 22:21:53 +0300

    cst_create_file: tmp_list_plans.py

---
+++ scripts/tmp_list_plans.py
@@ -0,0 +1,6 @@
+"""Temporary script to list plans directory."""
+
+# cst-node-ids: begin
+# cst-node-ids: version=2
+# cst-node-ids: data={"0":"0f887831-c4f2-43ea-bce2-7e3783aad462","0.0":"bfa85a94-5b21-4db1-bb8e-34ddf8a1894c","0.0.0":"9482342f-ae5d-4357-9c50-18b09f315974","0.0.0.0":"861ca22a-d7b9-438d-8ef0-7d3ee0360fc6","0.0.1":"b09fb690-1c49-4339-a27b-520836d72fe3","0.0.1.0":"70e64b17-f1b3-47b8-bc9c-63e1185da101","0.0.1.1":"5b6f06a0-addb-4d99-956b-2ec47b20d096"}
+# cst-node-ids: end

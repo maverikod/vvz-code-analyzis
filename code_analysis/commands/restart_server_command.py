ad721d5cb1d8a4cc940c7d0f2e70be64f7804162
Vasiliy Zdanovskiy <vasilyvz@gmail.com>
2026-04-30 20:56:50 +0300

    cst_save_tree: restart_server_command.py

--- code_analysis/commands/restart_server_command.py
+++ code_analysis/commands/restart_server_command.py
@@ -5,5 +5,5 @@ email: vasilyvz@gmail.com"""
 
 # cst-node-ids: begin
 # cst-node-ids: version=2
-# cst-node-ids: data={"0":"d39a995d-dea6-4d69-bbac-dac078627358","0.0":"4dc83724-e654-41b8-8da0-f4a933384682","0.0.0":"66636a52-e8e7-4774-afc0-511a3eea7de3","0.0.0.0":"0a529421-39fc-4f18-9011-7bc049ea8afb","0.0.1":"fd1f3bc4-4d52-44cb-8d3c-7a8332c35678","0.0.1.0":"4b72ccad-713c-433c-8c57-53c50cdf774d","0.0.1.1":"e4930a95-fb1a-4d52-9a39-3f8e5a2a3a57"}
+# cst-node-ids: data={"0":"d39a995d-dea6-4d69-bbac-dac078627358","0.0":"4dc83724-e654-41b8-8da0-f4a933384682"}
 # cst-node-ids: end

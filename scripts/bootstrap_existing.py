3f03030ab911f70a2d8c612bf2f8c4a5a9ccb6f7
Vasiliy Zdanovskiy <vasilyvz@gmail.com>
2026-05-05 14:15:21 +0300

    cst_save_tree: bootstrap_existing.py

--- scripts/bootstrap_existing.py
+++ scripts/bootstrap_existing.py
@@ -2,5 +2,5 @@
 
 # cst-node-ids: begin
 # cst-node-ids: version=2
-# cst-node-ids: data={"0":"c9d5af13-ed87-4bb2-8dd8-149f1eafea57","0.0":"4fe35b90-4960-438f-94ea-e79d90b4fa94","0.0.0":"d87bbc82-32d1-4351-ab60-3eab170841a7","0.0.0.0":"0e44010b-6fb8-4e96-a8ad-eb86d17d75e4","0.0.1":"24a27e50-f9c8-4767-bfd8-3799f2915c7e","0.0.1.0":"c58d7caf-9362-4051-b565-093cd6dd0d4c","0.0.1.1":"5cdf0003-4de6-489f-af6e-b3d44091093a"}
+# cst-node-ids: data={"0":"04728494-43c3-478c-96af-6926cd36dc8d","0.0":"4fe35b90-4960-438f-94ea-e79d90b4fa94","0.0.0":"d87bbc82-32d1-4351-ab60-3eab170841a7","0.0.0.0":"0e44010b-6fb8-4e96-a8ad-eb86d17d75e4","0.0.1":"24a27e50-f9c8-4767-bfd8-3799f2915c7e","0.0.1.0":"c58d7caf-9362-4051-b565-093cd6dd0d4c","0.0.1.1":"5cdf0003-4de6-489f-af6e-b3d44091093a"}
 # cst-node-ids: end

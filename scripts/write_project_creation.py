3488f2ff912dd685610d757feaf463b75dff6e31
Vasiliy Zdanovskiy <vasilyvz@gmail.com>
2026-05-05 13:40:07 +0300

    cst_create_file: write_project_creation.py

---
+++ scripts/write_project_creation.py
@@ -0,0 +1,6 @@
+"""Helper: overwrite project_creation.py with new bootstrapped version."""
+
+# cst-node-ids: begin
+# cst-node-ids: version=2
+# cst-node-ids: data={"0":"0e2c8a84-3f16-49b1-a432-3a6d80d7069a","0.0":"5382cefc-c9d8-4efb-a415-cc195efa7ae8","0.0.0":"f36fd8db-2cf8-4579-8c5a-d541dff4c07b","0.0.0.0":"abf1b055-e2ad-4ee1-96ab-38190bfae334","0.0.1":"038282d3-3bf6-4b49-95f0-29e8ad5ff022","0.0.1.0":"e5c75aaa-f225-4850-ac8d-761648383ad7","0.0.1.1":"522638b7-5d0e-49f6-8223-4c137e3a8e3b"}
+# cst-node-ids: end

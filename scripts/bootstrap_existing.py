b30ed55edb670aff31a4d79db26a08bce26ea4d5
Vasiliy Zdanovskiy <vasilyvz@gmail.com>
2026-05-05 14:14:53 +0300

    cst_create_file: bootstrap_existing.py

---
+++ scripts/bootstrap_existing.py
@@ -0,0 +1,6 @@
+"""Apply bootstrap to an existing project directory (scaffold + template + venv)."""
+
+# cst-node-ids: begin
+# cst-node-ids: version=2
+# cst-node-ids: data={"0":"c9d5af13-ed87-4bb2-8dd8-149f1eafea57","0.0":"4fe35b90-4960-438f-94ea-e79d90b4fa94","0.0.0":"d87bbc82-32d1-4351-ab60-3eab170841a7","0.0.0.0":"0e44010b-6fb8-4e96-a8ad-eb86d17d75e4","0.0.1":"24a27e50-f9c8-4767-bfd8-3799f2915c7e","0.0.1.0":"c58d7caf-9362-4051-b565-093cd6dd0d4c","0.0.1.1":"5cdf0003-4de6-489f-af6e-b3d44091093a"}
+# cst-node-ids: end

a379b52ddc059c2b8fd81ccb2dffdc8f51fe0564
Vasiliy Zdanovskiy <vasilyvz@gmail.com>
2026-05-06 23:22:21 +0300

    cst_create_file: _del_sidecar.py

---
+++ code_analysis/core/project_bootstrap/_del_sidecar.py
@@ -0,0 +1,7 @@
+import pathlib
+p = pathlib.Path('/home/vasilyvz/projects/tools/code_analysis/code_analysis/commands/.cst')
+if p.exists():
+    for f in p.glob('cst_modify_tree_command*'):
+        print('removing', f)
+        f.unlink()
+print('done')

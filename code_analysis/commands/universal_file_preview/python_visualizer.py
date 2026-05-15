5555ac7a2c176cc077257a3ab5ae400281428aed
Vasiliy Zdanovskiy <vasilyvz@gmail.com>
2026-05-15 03:23:48 +0300

    fs_remove: python_visualizer.py

--- code_analysis/commands/universal_file_preview/python_visualizer.py
+++
@@ -1,8 +0,0 @@
-"""Python node renderer for universal_file_preview (C-022).
-
-Converts CSTTree nodes to structured human-readable text for AI consumption.
-Two public functions: render_module (module overview) and render_node
-(single-node drill-down, to be added in the next step).
-
-Author: Vasiliy Zdanovskiy
-email: vasilyvz@gmail.com"""

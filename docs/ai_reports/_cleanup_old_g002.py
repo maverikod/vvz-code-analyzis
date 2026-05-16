0e7c17dda4fc178b0c14e8bec69dfdcde31e0b6f
Vasiliy Zdanovskiy <vasilyvz@gmail.com>
2026-05-17 02:55:34 +0300

    universal_file_save: _cleanup_old_g002.py

---
+++ docs/ai_reports/_cleanup_old_g002.py
@@ -0,0 +1,7 @@
+import shutil, pathlib
+p = pathlib.Path('/home/vasilyvz/projects/tools/code_analysis/docs/plans/2026-05-16-universal-file-edit/G-002-session-and-draft-model')
+if p.exists():
+    shutil.rmtree(p)
+    print('deleted')
+else:
+    print('not found')
